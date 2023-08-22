d5a7258faac093cea2c04e76587d7ed826266d78
Neo Su <neo.sh.su@fit-foxconn.com>
2023-08-22 21:58:32 +0800

    remove namespace

--- 8queenpuzzle/Program.cs
+++ 8queenpuzzle/Program.cs
@@ -1,94 +1,90 @@
 ﻿
-
-namespace EightQueensSolver
+class Program
 {
-    class Program
+    static int N = 8; // Number of queens and board size
+
+    static void Main(string[] args)
     {
-        static int N = 8; // Number of queens and board size
+        List<List<string>> solutions = SolveNQueens();
 
-        static void Main(string[] args)
+        Console.WriteLine($"Total distinct solutions: {solutions.Count}");
+        Console.WriteLine();
+
+        for (int i = 0; i < solutions.Count; i++)
         {
-            List<List<string>> solutions = SolveNQueens();
-
-            Console.WriteLine($"Total distinct solutions: {solutions.Count}");
+            Console.WriteLine($"Solution {i + 1}:\n");
+            PrintSolution(solutions[i]);
             Console.WriteLine();
+        }
+    }
 
-            for (int i = 0; i < solutions.Count; i++)
+    static List<List<string>> SolveNQueens()
+    {
+        List<List<string>> solutions = new List<List<string>>();
+        char[][] board = new char[N][];
+        for (int i = 0; i < N; i++)
+        {
+            board[i] = new char[N];
+            for (int j = 0; j < N; j++)
             {
-                Console.WriteLine($"Solution {i + 1}:\n");
-                PrintSolution(solutions[i]);
-                Console.WriteLine();
+                board[i][j] = '.';
             }
         }
 
-        static List<List<string>> SolveNQueens()
-        {
-            List<List<string>> solutions = new List<List<string>>();
-            char[][] board = new char[N][];
-            for (int i = 0; i < N; i++)
-            {
-                board[i] = new char[N];
-                for (int j = 0; j < N; j++)
-                {
-                    board[i][j] = '.';
-                }
-            }
+        SolveNQueensUtil(board, 0, solutions);
+        return solutions;
+    }
 
-            SolveNQueensUtil(board, 0, solutions);
-            return solutions;
+    static void SolveNQueensUtil(char[][] board, int col, List<List<string>> solutions)
+    {
+        if (col == N)
+        {
+            solutions.Add(CreateSolution(board));
+            return;
         }
 
-        static void SolveNQueensUtil(char[][] board, int col, List<List<string>> solutions)
+        for (int row = 0; row < N; row++)
         {
-            if (col == N)
+            if (IsSafe(board, row, col))
             {
-                solutions.Add(CreateSolution(board));
-                return;
-            }
-
-            for (int row = 0; row < N; row++)
-            {
-                if (IsSafe(board, row, col))
-                {
-                    board[row][col] = 'Q';
-                    SolveNQueensUtil(board, col + 1, solutions);
-                    board[row][col] = '.';
-                }
+                board[row][col] = 'Q';
+                SolveNQueensUtil(board, col + 1, solutions);
+                board[row][col] = '.';
             }
         }
+    }
 
-        static bool IsSafe(char[][] board, int row, int col)
+    static bool IsSafe(char[][] board, int row, int col)
+    {
+        for (int i = 0; i < col; i++)
         {
-            for (int i = 0; i < col; i++)
-            {
-                if (board[row][i] == 'Q')
-                    return false;
+            if (board[row][i] == 'Q')
+                return false;
 
-                if (row - i >= 0 && board[row - i][col - i] == 'Q')
-                    return false;
+            if (row - i >= 0 && board[row - i][col - i] == 'Q')
+                return false;
 
-                if (row + i < N && board[row + i][col - i] == 'Q')
-                    return false;
-            }
-            return true;
+            if (row + i < N && board[row + i][col - i] == 'Q')
+                return false;
         }
+        return true;
+    }
 
-        static List<string> CreateSolution(char[][] board)
+    static List<string> CreateSolution(char[][] board)
+    {
+        List<string> solution = new List<string>();
+        foreach (char[] row in board)
         {
-            List<string> solution = new List<string>();
-            foreach (char[] row in board)
-            {
-                solution.Add(new string(row));
-            }
-            return solution;
+            solution.Add(new string(row));
         }
+        return solution;
+    }
 
-        static void PrintSolution(List<string> solution)
+    static void PrintSolution(List<string> solution)
+    {
+        foreach (string row in solution)
         {
-            foreach (string row in solution)
-            {
-                Console.WriteLine(row);
-            }
+            Console.WriteLine(row);
         }
     }
 }
